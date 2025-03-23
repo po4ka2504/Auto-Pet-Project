aa04caa723f5ad7afac4edfafb8dc761fd11900b
maxpo4ka2504 <iwenko1704@gmail.com>
2025-03-23 16:04:29 +0100

    New tests etc

--- tests/testshop.spec.ts
+++ tests/testshop.spec.ts
@@ -16,18 +16,46 @@ test (`perform basic action`, async({page})=>{
 
     NavigationToTheResource;
 //locator.click()	Click the element
-p
+const topLinksMyaccount = await page.locator('#top-links').getByText('My Account').click() //found by using locators identifier 
+topLinksMyaccount;
+
+const topLinksMyAccountRegister = await page.getByRole(`link`,{name:`Register`}).click()
+topLinksMyAccountRegister;
+
+//locator.fill()	Fill the form field, input text
+const registerName = page.getByPlaceholder(`First Name`)
+
+await registerName.fill(`New User`)
+const registerLastName = page.getByPlaceholder(`Last Name`)
+
+//await registerLastName.fill(`New User Last name`) bug with input fielt 
+await registerLastName.fill(`123`)
+
+const registerEmail = page.getByPlaceholder(`E-Mail`).first()
 
 
-//locator.selectOption()	Select option in the drop down*/
+await registerEmail.fill(`valid_email@test.com`)
+
+const registerTelephone = page.getByLabel(`telephone`)
+const validphone = `123321111`
+await registerTelephone.fill(validphone)
+
+// locator.press()	Press single key
+const regiterPassword = page.getByPlaceholder(`Password`)
+const validPass = `test_12345`
+
+await regiterPassword.fill(validPass)
 
 
 
- /*
+const regiterPasswordConfirm = page.getByRole(`textbox`,{name:`Password Confirm`)
+
+await regiterPasswordConfirm.fill(validPass)
+
+/*
 
 locator.uncheck()	Uncheck the input checkbox
 locator.hover()	Hover mouse over the element
-locator.fill()	Fill the form field, input text
 locator.focus()	Focus the element
 locator.press()	Press single key
 locator.setInputFiles()	Pick files to upload
