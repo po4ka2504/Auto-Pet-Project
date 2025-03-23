bfa179870725f99b144f46954dfdbac1caedf2f8
maxpo4ka2504 <iwenko1704@gmail.com>
2025-03-23 17:56:05 +0100

    Registration e2e complete

--- tests/testshop.spec.ts
+++ tests/testshop.spec.ts
@@ -10,56 +10,67 @@ test (`navigate to the web page`, async({page})=>{
  await expect (page).toHaveTitle(`Best Friends`)
  
 })
+test.describe(`registration flow test suite`,() =>{
+
 
 test (`perform basic action`, async({page})=>{
-    const NavigationToTheResource = await page.goto(`http://opencart.qatestlab.net/`)
+const NavigationToTheResource = await page.goto(`http://opencart.qatestlab.net/`)
 
-    NavigationToTheResource;
+ NavigationToTheResource;
 //locator.click()	Click the element
 const topLinksMyaccount = await page.locator('#top-links').getByText('My Account').click() //found by using locators identifier 
 topLinksMyaccount;
-
 const topLinksMyAccountRegister = await page.getByRole(`link`,{name:`Register`}).click()
 topLinksMyAccountRegister;
-
 //locator.fill()	Fill the form field, input text
 const registerName = page.getByPlaceholder(`First Name`)
-
 await registerName.fill(`New User`)
 const registerLastName = page.getByPlaceholder(`Last Name`)
-
 //await registerLastName.fill(`New User Last name`) bug with input fielt 
 await registerLastName.fill(`123`)
-
 const registerEmail = page.getByPlaceholder(`E-Mail`).first()
 
+function generateUniqueValidEmail() {
+    const timestamp = new Date().getTime();
+    return `user_${timestamp}@test.com`;
+  }
+  let generateUniqueValidEmailValue =generateUniqueValidEmail()
 
-await registerEmail.fill(`valid_email@test.com`)
+  await registerEmail.fill(generateUniqueValidEmailValue)
 
 const registerTelephone = page.getByLabel(`telephone`)
 const validphone = `123321111`
 await registerTelephone.fill(validphone)
-
-// locator.press()	Press single key
 const regiterPassword = page.getByPlaceholder(`Password`)
 const validPass = `test_12345`
-
 await regiterPassword.fill(validPass)
-
-
-
-const regiterPasswordConfirm = page.getByRole(`textbox`,{name:`Password Confirm`)
-
+const regiterPasswordConfirm = page.getByRole(`textbox`,{name:`Password Confirm`})
 await regiterPasswordConfirm.fill(validPass)
 
-/*
+const registerNewsleterYes = page.getByText('Yes', { exact: true })
+const registerNewsleterNo = page.getByText('No', { exact: true })
 
+await registerNewsleterYes.check()
+await registerNewsleterNo.check()
+await registerNewsleterYes.check()
+
+const registerRadio = page.getByRole(`radio`)
+
+await registerRadio.click()
+
+const registerButton = page.getByRole(`button`,{name:`Continue`})
+
+await registerButton.click()
+
+const finalScreen =page.getByRole(`heading`,{name:`Your Account Has Been Created!`})
+await expect(finalScreen).toBeVisible()
+/*
+// locator.press()	Press single key
 locator.uncheck()	Uncheck the input checkbox
 locator.hover()	Hover mouse over the element
 locator.focus()	Focus the element
 locator.press()	Press single key
 locator.setInputFiles()	Pick files to upload
 */
-
-
+})
 })
